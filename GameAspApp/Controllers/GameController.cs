2cdbd4205deb9e988f8661718c73df2eb37d4ed7
Dmitry Kramarenko <KramarenkoDP@yandex.ru>
2020-11-19 01:15:47 +0400

    TASK-1 done

---
+++ GameAspApp/Controllers/GameController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameAspApp.Common.Swagger;
+
+namespace GameAspApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [ApiExplorerSettings(GroupName = SwaggerDocParts.Games)]
+    public class GameController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Game> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Game
+            {
+                ReleaseDate = DateTime.Now.AddDays(index),
+                Name = index.ToString(),
+                Developer = index.ToString(),
+                Publisher = index.ToString(),
+                Metascore = rng.Next(1, 10),
+            })
+            .ToArray();
+        }
+    }
+}
